e2d36c34a61f785094c97ab2febf73c3a91aa21a
Fingle <LordFingle@gmail.com>
2016-10-16 12:21:10 +1000

    Remove obsolete show_settingsbar setting

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -91,7 +91,7 @@ void set_default_settings(Settings *settings)
 	#else
 	settings->setDefault("show_debug", "true");
 	#endif
-	settings->setDefault("show_settingsbar", "false");
+ 
 	settings->setDefault("show_homebutton", "true");
 
 	settings->setDefault("fps_max", "60");
