b255652bb84bafae29184c2a5d60b01c3c31363a
Fingle <LordFingle@gmail.com>
2016-10-21 21:34:21 +1000

    Implement a better fullscreen mode, including preventing back button menu

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -11,10 +11,33 @@ public class MtNativeActivity extends NativeActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
 		m_MessagReturnCode = -1;
 		m_MessageReturnValue = "";
-
+		makeFullScreen();
 	}
+
+	public void makeFullScreen() {
+        if (Build.VERSION.SDK_INT >= 19) {
+            this.getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            makeFullScreen();
+        }
+    }
+
+	@Override
+    public void onBackPressed() {
+    }
 
 	@Override
 	public void onDestroy() {
