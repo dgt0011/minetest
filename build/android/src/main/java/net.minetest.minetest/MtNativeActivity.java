096698927982946eecabf0517af3d1f9f78368cb
Fingle <LordFingle@gmail.com>
2016-10-22 15:21:56 +1000

    Suppress back button

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -42,6 +42,15 @@ public class MtNativeActivity extends NativeActivity {
     }
 
 	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+     if (keyCode == KeyEvent.KEYCODE_BACK) {
+     //preventing default implementation previous to android.os.Build.VERSION_CODES.ECLAIR
+     return true;
+     }
+     return super.onKeyDown(keyCode, event);    
+	}
+
+	@Override
 	public void onDestroy() {
 		super.onDestroy();
 	}
