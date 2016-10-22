9633ed2cac0429776f0c70fb93adfb8f3d5c7e20
Fingle <LordFingle@gmail.com>
2016-10-22 13:55:59 +1000

    Fix compile errors

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -2,8 +2,10 @@ package net.minetest.minetest;
 
 import android.app.NativeActivity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.net.Uri;
 
