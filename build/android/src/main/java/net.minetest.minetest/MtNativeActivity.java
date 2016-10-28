2824014bffadbb7f3349f44c2a3a14506ff8c2d9
Fingle <LordFingle@gmail.com>
2016-10-29 09:26:16 +1000

    Add necessary import statements.

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -9,6 +9,15 @@ import android.view.View;
 import android.view.WindowManager;
 import android.net.Uri;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import android.content.res.AssetFileDescriptor;
+
 import android.speech.tts.TextToSpeech;
 import java.util.Locale;
 
