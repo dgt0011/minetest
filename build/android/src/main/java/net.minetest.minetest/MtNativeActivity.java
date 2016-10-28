adbf885ebac286b65cebd7cbd0e349926c0834b3
Fingle <LordFingle@gmail.com>
2016-10-28 20:33:15 +1000

    Fix syntax error

--- build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
+++ build/android/src/main/java/net.minetest.minetest/MtNativeActivity.java
@@ -36,7 +36,7 @@ public class MtNativeActivity extends NativeActivity {
             public void onInit(int status) {
                 
                 if(status == TextToSpeech.SUCCESS){
-                    int result=tts.setLanguage(Locale.US);
+                    int result=t1.setLanguage(Locale.US);
                     if(result==TextToSpeech.LANG_MISSING_DATA ||
                             result==TextToSpeech.LANG_NOT_SUPPORTED){
                         Log.e("error", "This Language is not supported");
